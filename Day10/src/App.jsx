c8a3c0f6ad88ca9c829cab281a4d7a061ad87b19
abdalegurey <[200~abdalegureyguled@gmail.com~>
2025-04-17 17:15:59 +0300

    adding Day10

---
+++ Day10/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Gallery from './Components/Gallery'
+
+const App = () => {
+  return (
+    <div>
+
+      <Gallery/>
+      
+    </div>
+  )
+}
+
+export default App
